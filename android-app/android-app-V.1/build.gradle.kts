74777387c08c0181a2655a7fc6936f17ba0cae58
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-08-19 12:10:10 -0600

    changed file styructre

--- android-app/android-app-V.1/build.gradle.kts
+++ android-app/android-app-V.1/build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    // Add global plugins here if needed
 }
 
-android {
-    namespace = "com.networkedcapital.rep"
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
     compileSdk = 34
 
     defaultConfig {
