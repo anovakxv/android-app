76b9475cdfb3eb089570ce038d5ac7959aae9c98
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-08-27 16:41:43 -0600

    changed main activity

--- android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/data/api/MessagingApiService.kt
+++ android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/data/api/MessagingApiService.kt
@@ -5,21 +5,22 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface MessagingApiService {
+    
     @GET(ApiConfig.MESSAGES_CONVERSATIONS)
     suspend fun getConversations(): Response<List<Conversation>>
-
+    
     @GET(ApiConfig.MESSAGES_LIST)
     suspend fun getMessages(@Query("user_id") userId: String): Response<List<Message>>
-
+    
     @POST(ApiConfig.MESSAGES_SEND)
     suspend fun sendMessage(@Body request: SendMessageRequest): Response<Message>
-
+    
     @DELETE(ApiConfig.MESSAGES_DELETE)
     suspend fun deleteMessage(@Query("message_id") messageId: String): Response<Unit>
-
+    
     @PUT("api/messaging/mark_read")
     suspend fun markAsRead(@Body request: MarkReadRequest): Response<Unit>
-
+    
     @GET("api/messaging/unread_count")
     suspend fun getUnreadCount(): Response<UnreadCountResponse>
 }
@@ -39,12 +40,6 @@ data class SendMessageRequest(
 
 data class MarkReadRequest(
     val userId: String // The user whose messages to mark as read
-)
-
-data class UnreadCountResponse(
-    val totalUnreadCount: Int,
-    val conversationCounts: Map<String, Int> // userId to unread count
-)
 )
 
 data class UnreadCountResponse(
