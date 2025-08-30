8fd7bedbf45146bc1930e080d676ea70d4c44890
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-08-30 15:06:30 -0600

    changed build to fix errors in start up

---
+++ android-app/android-app-V.1/build.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rep"
+include(":app")
+
+// Add this block at the TOP of your project-level build.gradle.kts (not the app/build.gradle!):
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
