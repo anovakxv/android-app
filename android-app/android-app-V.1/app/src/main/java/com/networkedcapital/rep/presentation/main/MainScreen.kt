23d2301a33a922f99dd3f7bf757b48a1aeb94b06
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-08-31 14:40:38 -0600

    Update MainScreen.kt

--- android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/presentation/main/MainScreen.kt
+++ android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/presentation/main/MainScreen.kt
@@ -234,15 +234,15 @@ fun MainScreen(
                         onClick = {
                             if (uiState.activeChats.isNotEmpty()) {
                                 val firstActiveChat = uiState.activeChats.first()
-                                val id = firstActiveChat.id
+                                val id = firstActiveChat.id // This 'id' could be of various types
                                 when (id) {
                                     is Int -> {
-                                        onNavigateToChat(id)
+                                        onNavigateToChat(id) // Expects an Int
                                     }
                                     is String -> {
                                         val intId = id.toIntOrNull()
                                         if (intId != null) {
-                                            onNavigateToChat(intId)
+                                            onNavigateToChat(intId) // Expects an Int
                                         } else {
                                             Log.e("MainScreen", "Failed to convert chat ID to Int: $id")
                                         }
@@ -255,7 +255,7 @@ fun MainScreen(
                         }
                     ) {
                         Badge(
-                            modifier = Modifier.offset(x = 8.dp, y = (-8).dp)
+                            modifier = Modifier.offset(x = 8.dp, y = (-8).dp) // Line 239
                         ) {
                             if (uiState.activeChats.isNotEmpty()) {
                                 Text(
