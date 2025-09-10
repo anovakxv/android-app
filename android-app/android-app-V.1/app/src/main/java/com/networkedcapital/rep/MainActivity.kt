9e6ee7ce7a17ca2de1913693ea7514505c514571
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-09-10 15:21:21 -0600

    Update MainActivity.kt

--- android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/MainActivity.kt
+++ android-app/android-app-V.1/app/src/main/java/com/networkedcapital/rep/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.draw.clip
 
 // ---------------------- SEGMENTED PICKER ----------------------
 @Composable
