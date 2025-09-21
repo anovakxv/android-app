b4df5865fd5e0e68c8badb775e16f9d9b936cbf9
tntsus97 <82912454+tntsus79@users.noreply.github.com>
2025-09-21 12:09:40 -0600

    changed to live api

--- android-app/android-app-V.1/python-backend/config.py
+++ android-app/android-app-V.1/python-backend/config.py
@@ -1,4 +1,6 @@
+
 class Config:
     SECRET_KEY = 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASE_API_URL = 'https://rep-june2025.onrender.com'
